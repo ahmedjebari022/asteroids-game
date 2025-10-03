5ff2b77d1c8c00dc6e277dbf95d92c1363a5173b
jebari ahmed kacem <ahmedkacem.jebari@esprit.tn>
2025-10-03 18:06:03 +0100

    - Add main game loop in main.py with pygame setup - Implement Player class with movement, rotation, and custom draw method - Create CircleShape base class for game objects - Define game constants in constants.py - Add pyproject.toml for dependencies and project metadata - Add .python-version and .gitignore for environment management

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT,ASTEROID_MIN_RADIUS,ASTEROID_KINDS,ASTEROID_SPAWN_RATE,ASTEROID_MAX_RADIUS
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT,ASTEROID_MIN_RADIUS,ASTEROID_KINDS,ASTEROID_SPAWN_RATE,ASTEROID_MAX_RADIUS,PLAYER_RADIUS,PLAYER_TURN_SPEED
+from player import Player
 def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     color = ""
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
+    
     print("Starting Asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
@@ -12,7 +20,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # player.update(dt)
+        updatable.update(dt)
+
         screen.fill("black")
+        # player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
